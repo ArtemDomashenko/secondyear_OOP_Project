0d4b564eab2a9829092a981d730c18518226a3d5
ArtemDomashenko13 <S00272372@atu.ie>
2026-02-15 21:40:15 +0000

    first commit xaml and app.xaml

---
+++ Project_WPF/Project_WPF/Mvvm/NotifyBase.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Project_WPF.Mvvm
+{
+    public class NotifyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string name = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(name);
+            return true;
+        }
+    }
+}
